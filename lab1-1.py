577192be1a6bd9eb9535e34167445eddcf8a4375
PanosDast <ece8035@upnet.gr>
2023-03-01 15:37:47 +0200

    Print headers

--- lab1-1.py
+++ lab1-1.py
@@ -1,19 +1,8 @@
 import requests  # εισαγωγή της βιβλιοθήκης
 
-
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
-
-
 url = input('Enter url:')   # προσδιορισμός του url
 
-with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
+with requests.head(url) as response:  # το αντικείμενο response
+	print(response.headers)
+
+input("Press Enter to continue...")
