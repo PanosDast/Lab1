1cf91d7e29462a11f5a2c14f1de7eb0ac0c357b1
PanosDast <ece8035@upnet.gr>
2023-03-01 17:00:08 +0200

    Print specific headers

--- lab1-1.py
+++ lab1-1.py
@@ -1,8 +1,16 @@
 import requests  # εισαγωγή της βιβλιοθήκης
+from datetime import datetime
 
-url = input('Enter url:')   # προσδιορισμός του url
+url = input('Input url:')   # προσδιορισμός του url
 
-with requests.head(url) as response:  # το αντικείμενο response
-	print(response.headers)
+with requests.get(url) as response:  # το αντικείμενο response
+	print(response.headers)          # print headers
+	print('Server software:', response.headers['server']) #print server software
+	
+	if response.cookies:      #check cookies
+	    for c in response.cookies:  #print cookie names
+		       print('Cookie:', c.name +"->", c.value)
+		       if c.expires:        #print cookie expiry date
+		           print('Cookie valid until:', datetime.fromtimestamp(c.expires))
 
 input("Press Enter to continue...")
